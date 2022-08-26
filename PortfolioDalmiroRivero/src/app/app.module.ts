9f1223eb0e3e70894dd305554cd50abd4977ee86
Dalmiro Rivero <dalmiro.rivero1@gmail.com>
2022-08-26 02:41:49 -0300

    HOME/HEADER

--- PortfolioDalmiroRivero/src/app/app.module.ts
+++ PortfolioDalmiroRivero/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
